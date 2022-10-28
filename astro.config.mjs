969848c0d17505727a84b60df6a9b7f0de10669a
Iven Beck <ivenbeck@outlook.de>
2022-10-28 20:13:22 +0200

    Add markdown math support, fix layout on edge case screen sizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import rehypeMathJax from "rehype-mathjax";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJax],
+  },
+});
